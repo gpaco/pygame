070f461bc3418eb635b063b91b81106093ff5e83
J. Rene Ortega Jr <atlasbusinessmarketing@gmail.com>
2018-12-29 16:09:59 -0800

    changed default speeds of paddles and ball

--- tetris.py
+++ tetris.py
@@ -146,9 +146,19 @@ class Piece(object):
     def __init__(self, x, y, shape):
         self.x = x
         self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
 
-def create_grid(locked_positions={}):
-    pass
+def create_grid(locked_pos = {}):
+    grid = [[(0,0,0) for x in range(10)] for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                c = locked_pos[(j, i)]
+                grid[i][j] = c
+    return grid
 
 def convert_shape_format(shape):
     pass
@@ -160,14 +170,17 @@ def check_lost(positions):
     pass
 
 def get_shape():
-    pass
-
+    return Peice(5, 0, random.choice(shapes))
 
 def draw_text_middle(text, size, color, surface):
     pass
 
-def draw_grid(surface, row, col):
-    pass
+def draw_grid(surface, grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 4)
 
 def clear_rows(grid, locked):
     pass
@@ -175,11 +188,41 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
     pass
 
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill((0,0,0))
+
+    pygame.font.init()
+    font = pygame.font.SYSFont('comicsans', 60)
+    label = font.render('Tetris', 1, (255,255,255))
+
+    surface.blit(label, (top_left_x + play_width/2 - label.get_width()/2), 30)
+    draw_grid(surface, grid)
+    pygame.display.update()
 
 def main():
-    pass
+
+    locked_pos = {}
+    grid = create_grid(locked_pos)
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
 
 def main_menu():
     pass
